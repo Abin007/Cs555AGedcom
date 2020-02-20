32a8afe7410d84f782d08b83cdcb7e972b80449f
prateekJani <58832930+prateekJani@users.noreply.github.com>
2020-02-20 02:36:55 -0500

    Update storytest.py

--- storytest.py
+++ storytest.py
@@ -1,5 +1,6 @@
 import unittest
 import validator
+from prettytable import PrettyTable
 
 
 class fooTest(unittest.TestCase):
@@ -10,10 +11,23 @@ class fooTest(unittest.TestCase):
         self.assertEqual(validator.StoryIDUS25(),"family Id F4 has same name - ['Robert', 'Kris', 'Kim', 'Khloé', 'Kourtney', 'Kim']")
     
     def test_story_us30(self):
-        print("Testing in progress")
+        x = PrettyTable()
+        x.field_names = ['Husband ID', 'Husband Name', 'Wife ID', 'Wife Name']
+        x.add_row(['I2', 'Kanye /West/', 'I1', 'Kim /Kardashian/'])
+        x.add_row(['I7', 'Bruce /Jenner/', 'I6', 'Kris /Jenner/'])
+        x.add_row(['I13', 'Travis /Scott/', 'I12', 'Kylie Jenner'])
+        self.assertEqual(validator.StoryIDUS30(), x)
     
     def test_story_us31(self):
-        print("Testing in progress")
+        x = PrettyTable()
+        x.field_names = ['ID','Name']
+        x.add_row(['I8', 'KhloÃ© /Kardashian/'])
+        x.add_row(['I9', 'Kourtney /kardashian/'])
+        x.add_row(['I10', 'Saint /West/'])
+        x.add_row(['I11', 'North /West/'])
+        x.add_row(['I14', 'Kendall /Jenner/'])
+        x.add_row(['I15', 'Stormi /Webster/'])
+        self.assertEqual(validator.StoryIDUS31(), x)
 
 
 if __name__=='__main__':
