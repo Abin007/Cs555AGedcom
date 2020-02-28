92ec969aa1cc8fdca65d86e47b4bf5111c85c3d6
prateekJani <58832930+prateekJani@users.noreply.github.com>
2020-02-27 20:03:12 -0500

    Update storytest.py

--- storytest.py
+++ storytest.py
@@ -9,32 +9,44 @@ class fooTest(unittest.TestCase):
 
     def test_story_us25(self):
         us25=set()
-        self.assertEqual(validator.StoryIDUS25(),['US25 - Error : Individual I1 I16 might be the same  in Family F4'] )
+        self.assertEqual(validator.StoryIDUS25(),['US25 - Error : Individual I1 I16 might be the same  in Family F4'])
     
     def test_story_us30(self):
         x = PrettyTable()
         x.field_names = ['Husband ID', 'Husband Name', 'Wife ID', 'Wife Name']
         x.add_row(['I2', 'Kanye /West/', 'I1', 'Kim /Kardashian/'])
         x.add_row(['I7', 'Bruce /Jenner/', 'I6', 'Kris /Jenner/'])
-        x.add_row(['I13', 'Travis /Scott/', 'I12', 'Kylie Jenner'])
-        self.assertTrue(validator.StoryIDUS30(), x)
+        x.add_row(['I13', 'Travis /Scott/', 'I12', 'Kylie /Jenner/'])
+        self.assertEqual(str(validator.StoryIDUS30()), str(x))
         y = PrettyTable()
         y.field_names = ['Husband ID', 'Husband Name', 'Wife ID', 'Wife Name']
         y.add_row(['I20', 'Kanye /West/', 'I1', 'Kim /Kardashian/'])
         y.add_row(['I7', 'Bruce /Jenner/', 'I6', 'Kris /Jenner/'])
-        y.add_row(['I13', 'Prateek /Jani/', 'I12', 'Kylie Jenner'])
-        self.assertFalse(validator.StoryIDUS30(), y)
+        y.add_row(['I13', 'Prateek /Jani/', 'I12', 'Kylie /Jenner/'])
+        self.assertNotEqual(str(validator.StoryIDUS30()), str(y))
     
     def test_story_us31(self):
         x = PrettyTable()
         x.field_names = ['ID','Name']
         x.add_row(['I8', 'KhloÃ© /Kardashian/'])
-        x.add_row(['I9', 'Kourtney /kardashian/'])
+        x.add_row(['I9', 'Kourtney /Kardashian/'])
         x.add_row(['I10', 'Saint /West/'])
         x.add_row(['I11', 'North /West/'])
         x.add_row(['I14', 'Kendall /Jenner/'])
         x.add_row(['I15', 'Stormi /Webster/'])
-        self.assertTrue(validator.StoryIDUS31(), x)
+        x.add_row(['I16', 'Kim /Kardashian/'])
+        self.assertEqual(str(validator.StoryIDUS31()), str(x))
+        
+        y = PrettyTable()
+        y.field_names = ['ID','Name']
+        y.add_row(['I8', 'KhloÃ© /Kardashian/'])
+        y.add_row(['I9', 'Kourtney /Kardashian/'])
+        y.add_row(['I10', 'Saint /West/'])
+        y.add_row(['I11', 'North /West/'])
+        y.add_row(['I14', 'Kendall /Jenner/'])
+        y.add_row(['I15', 'Prateek /Jani/'])
+        y.add_row(['I16', 'Kim /Kardashian/'])
+        self.assertNotEqual(str(validator.StoryIDUS31()),str(y))
 
     def test_story_us01(self):
         self.assertEqual(validator.StoryIDUS01(),['US01 - Error : Individual ID - I15 Birthday 3 MAR 2021 occurs in the future'])
